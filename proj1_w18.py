3af72749cca4cbc515697ccd1cca06ec0c5c01f7
Anndo Ko <handcuffanndo@gmail.com>
2018-01-25 16:00:42 -0500

    implement the Media class

--- proj1_w18.py
+++ proj1_w18.py
@@ -1,9 +1,16 @@
 
 class Media:
 
-	def __init__(self, title="No Title", author="No Author"):
+	def __init__(self, title="No Title", author="No Author", year = "No Year"):
 		self.title = title
 		self.author = author
+		self.release_year = year
+
+	def __str__(self):
+		return "{} by {} ({})".format(self.title, self.author, self.release_year)
+
+	def __len__(self):
+		return 0
 
 ## Other classes, functions, etc. should go here
 
