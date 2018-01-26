43d6f35c27dbd7a9a2b0233d6fd8e6a93472be0e
Anndo Ko <handcuffanndo@gmail.com>
2018-01-26 00:00:15 -0500

    create objects from JSON

--- proj1_w18.py
+++ proj1_w18.py
@@ -1,28 +1,44 @@
 import math # for length calculation
+import requests
+import json
 
 # Media
 class Media:
 
-	def __init__(self, title = "No Title", author = "No Author", year = "No Year"):
-		self.title = title
-		self.author = author
-		self.release_year = year
+	def __init__(self, title = "No Title", author = "No Author", year = "No Year", json_dic = None):
+		if json_dic is None:
+			self.title = title
+			self.author = author
+			self.release_year = year
+		elif json_dic["wrapperType"] != "track":
+			self.title = json_dic["collectionName"]
+			self.author = json_dic["artistName"]
+			self.release_year = json_dic["releaseDate"][0:4]
+		else:
+			self.title = json_dic["trackName"]
+			self.author = json_dic["artistName"]
+			self.release_year = json_dic["releaseDate"][0:4]
 
 	def __str__(self):
 		return "{} by {} ({})".format(self.title, self.author, self.release_year)
 
 	def __len__(self):
-		return 0
+		return "N/A"
 
 ## Other classes, functions, etc. should go here
 # Song (subclass of Media)
 class Song(Media):
 
-	def __init__(self, title = "No Title", author = "No Author", year = "No Year", album = "No Album", genre = "No Genre", track_len = 0):
-		super().__init__(title, author, year)
-		self.album = album
-		self.genre = genre
-		self.len = track_len
+	def __init__(self, title = "No Title", author = "No Author", year = "No Year", album = "No Album", genre = "No Genre", track_len = "N/A", json_dic = None):
+		super().__init__(title, author, year, json_dic)
+		if json_dic is None:
+			self.album = album
+			self.genre = genre
+			self.len = track_len
+		else:
+			self.album = json_dic["collectionName"]
+			self.genre = json_dic["primaryGenreName"]
+			self.len = json_dic["trackTimeMillis"]
 
 	def __str__(self):
 		return super().__str__() + " [{}]".format(self.genre)
@@ -34,10 +50,14 @@ class Song(Media):
 # Movie (subclass of Media)
 class Movie(Media):
 
-	def __init__(self, title = "No Title", author = "No Author", year = "No Year", rating = "No Rating", movie_len = 0):
-		super().__init__(title, author, year)
-		self.rating = rating
-		self.len = movie_len
+	def __init__(self, title = "No Title", author = "No Author", year = "No Year", rating = "No Rating", movie_len = "N/A", json_dic = None):
+		super().__init__(title, author, year, json_dic)
+		if json_dic is None:
+			self.rating = rating
+			self.len = movie_len
+		else:
+			self.rating = json_dic["contentAdvisoryRating"]
+			self.len = json_dic["trackTimeMillis"]
 
 	def __str__(self):
 		return super().__str__() + " [{}]".format(self.rating)
@@ -45,6 +65,22 @@ class Movie(Media):
 	def __len__(self):
 		len_in_mins = math.ceil(self.len / 1000 / 60) # round time to the nearest minute
 		return len_in_mins
+
+# test the code
+# use sample_json.json
+data = json.load(open('sample_json.json'))
+m1 = Movie(json_dic = data[0])
+s1 = Song(json_dic = data[1])
+o1 = Media(json_dic = data[2])
+print(m1)
+print(s1)
+print(o1)
+print(m1.__str__())
+print(s1.__str__())
+print(o1.__str__())
+print(m1.__len__())
+print(s1.__len__())
+print(o1.__len__())
 
 if __name__ == "__main__":
 	# your control code for Part 4 (interactive search) should go here
